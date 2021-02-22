44b221f822c4ce7bb3765c0b01327bd2b69f20ea
batuhan öztürk <batuhandoruk91@gmail.com>
2021-02-22 23:43:59 +0200

    initial commit excel file upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# shopping-guide
+
+This project is designed to track your shopping history of users and store goods and prices of products to give users an idea of where to shop for each item to provide the most wallet-friendly option.
+
+## Initiation
+
+  Initially, it would help to track my shopping history based on my shopping in the last months.
---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const formidable = require('formidable');
+const readXlFile = require('read-excel-file/node');
+
+
+app.set('view engine', 'ejs')
+
+const dataApp = [];
+
+app.route('/')
+  .get((req, res) => {
+    res.render('home');
+  })
+  .post((req, res) => {
+    const form = formidable({
+      multiples: true
+    });
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.json({fields, files});
+      console.log(files.someExpressFiles.path)
+      readXlFile(files.someExpressFiles.path).then((rows) => {
+        console.log(rows);
+      }).catch(err => console.log(err));
+    });
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port: ${process.env.PORT}`);
+});
